5d5aa7c163b4231d7cde60a97b4802a7f4685353
reiwang2023 <zmnwrs@gmail.com>
2023-08-16 00:24:30 +1000

    Cannot get google-find place Cannot make marker in Google map
    failed

--- final-project/src/components/App/index.js
+++ final-project/src/components/App/index.js
@@ -1,34 +1,104 @@
-import {useEffect} from 'react'
-const API_KEY = 'AIzaSyCUq9TdIsCUNEYgfZB2AmQm2jjwbgPsJXY'
-//process.env.REACT_APP_OPEN_WEATHER_API_KEY
+import { useEffect } from 'react'
+import { useState } from 'react'
+import MapContainer from "../Marker/marker"
+
+
 const App = () => {
+
+  const API_KEY = 'AIzaSyCUq9TdIsCUNEYgfZB2AmQm2jjwbgPsJXY'
+  //process.env.REACT_APP_OPEN_WEATHER_API_KEY
+
+  const [searchPrompt, setPrompt] = useState('')
+  const [map, setMap] = useState('')
+  const [searchResult, setSearchResult] = useState('')
+
+
+  function handleChange(e) {
+    setPrompt(e.target.value)
+  }
+
   const fetchMap = () => {
-    fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${API_KEY}&&location=-37.81218719482422,144.96229553222656&radius=1000&type=restaurant`).then(data=> {
-      return data.json()
-    }).then(jsonData => {
-     console.log(jsonData.results)
-    }).catch(error=> {
-      console.log(error);
+    fetch(
+      //`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCUq9TdIsCUNEYgfZB2AmQm2jjwbgPsJXY&&location=-37.81218719482422,144.96229553222656&radius=1500&type=restaurant$keywords=${searchPrompt}`, 
+      //{ headers: { "Access-Control-Allow-Origin": "*" }, mode: 'no-cors' }
+      `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&input=${searchPrompt}&inputtype=textquery&locationbias=circle%3A2000%40-37.81218719482422%2C144.96229553222656&key=${API_KEY}`, {
+      method: "GET",
+      mode: "no-cors"
     })
+    .then((data) =>{
+      console.log(data.body)
+    })
+    // ).then((res) => res.json())
+      // .then((data) => {
+      //   console.log(data);
+      //   setSearchResult(data.candidates);
+      //   console.log(searchResult)
+      //   setMap(data.candidates.formatted_address)
+      // })
+      // .catch(error => {
+      //   console.log(error);
+      // })
   }
   useEffect(() => {
-    fetchMap()
-  }, [])
+    //fetchMap()
+    initmap()
+  }, [fetchMap])
+
+  const initmap = () => {
+    var sydney = new google.maps.LatLng(-33.867, 151.195);
+  
+    infowindow = new google.maps.InfoWindow();
+  
+    map = new google.maps.Map(
+        document.getElementById('map'), {center: sydney, zoom: 15});
+  
+    var request = {
+      query: 'Museum of Contemporary Art Australia',
+      fields: ['name', 'geometry'],
+    };
+  
+    var service = new google.maps.places.PlacesService(map);
+  
+    service.findPlaceFromQuery(request, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+        map.setCenter(results[0].geometry.location);
+      }
+    });
+    setMap(map)
+  }
+
   return (
     <>
-      <p>Rei Wang</p>
+      <p>Rei Wang - Meals to try</p>
+      <input placeholder="Find a restaurant " type='text' value={searchPrompt} onChange={handleChange} />
+      <button onClick={fetchMap}>Find a restaurant</button>
+      <br />
+
+      <p>This input is {searchPrompt}</p>
+      <p>The output info should be {map}   --- Can make it into a to-do list item </p>
+      <br />
+      <p>This is the Melbourne CBD map---to make markers here</p>
+
+      <br />
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.8195613507864!3d-6.194741395493371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5390917b759%3A0x6b45e67356080477!2sPT%20Kulkul%20Teknologi%20Internasional!5e0!3m2!1sen!2sid!4v1601138221085!5m2!1sen!2sid"
+        title="Melboure CBD map"
         width="600"
         height="450"
-        frameBorder="0"
         style={{ border: 0 }}
-        allowFullScreen=""
-        aria-hidden="false"
-        tabIndex="0"
-      />
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCUq9TdIsCUNEYgfZB2AmQm2jjwbgPsJXY
+    &q=Melbourne 3000">
+      </iframe>
       <br />
+      <p>Test for Marker</p>
+      <MapContainer />
     </>
   )
 }
+
 export default App;
